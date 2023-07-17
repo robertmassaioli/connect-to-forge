1554e4f7b651cea3a17417a3f07fe59440220e04
Robert Massaioli <rmassaioli@atlassian.com>
2023-07-17 13:25:05 +1000

    Updated the readme and the output of the cli tool.

---
+++ README.md
@@ -0,0 +1,10 @@
+## connect-to-forge
+
+This tool is designed to let Connect developers quickly migrate their apps to Forge, starting with the Descriptor to Manifest conversion. To use this tool, simply:
+
+``` bash
+yarn install
+yarn start --type <jira|confluence> --url https://website.com/path/to/descriptor.json
+```
+
+Where the type variable represents the Atlassian product that this app is meant to be installed into and the URL is somewhere on the public internet that the descriptor can be downloaded from.
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import yaml from 'js-yaml';
 import { program } from 'commander';
 import inquirer from 'inquirer';
 import { error } from 'console';
+import { isPresent } from 'ts-is-present';
 
 // Typings for Atlassian Connect Descriptor
 interface ConnectDescriptor {
+  name: string;
   key: string;
   baseUrl: string;
   scopes: string[];
@@ -35,6 +37,8 @@ program
   .requiredOption('-u, --url <url>', 'Atlassian Connect descriptor URL')
   .option('-t, --type <type>', 'App type (jira or confluence)')
   .option('-o, --output <path>', 'Output file path', 'manifest.yml')
+  .name('connect-to-forge')
+  .usage('--type <jira|confluence> --url https://website.com/path/to/descriptor.json')
   .parse(process.argv);
 
 const { url, type, output } = program.opts();
@@ -74,14 +78,22 @@ function convertToForgeManifest(connect: ConnectDescriptor, type: 'jira' | 'conf
 
   let warnings: string[] = [];
 
+  console.log(`Conversion begun for '${connect.name}':`);
+  console.log('');
+
   // Add lifecycle events
   if (connect.lifecycle) {
-    manifest.connectModules[`${type}:lifecycle`] = { key: 'lifecycle-events', ...connect.lifecycle };
+    const moduleName = `${type}:lifecycle`;
+    manifest.connectModules[moduleName] = { key: 'lifecycle-events', ...connect.lifecycle };
+    console.log(` - Moved all lifecycle events into connectModules.${moduleName}.`);
   }
 
   // Add modules
-  for (const [moduleType, moduleContent] of Object.entries(connect.modules)) {
-    manifest.connectModules[`${type}:${moduleType}`] = moduleContent;
+  if (connect.modules) {
+    for (const [moduleType, moduleContent] of Object.entries(connect.modules)) {
+      manifest.connectModules[`${type}:${moduleType}`] = moduleContent;
+    }
+    console.log(` - Moved ${Object.keys(connect.modules).length} modules into connectModules in the Manifest`);
   }
 
   // Add webhooks with keys
@@ -90,18 +102,23 @@ function convertToForgeManifest(connect: ConnectDescriptor, type: 'jira' | 'conf
     webhooks.forEach((webhook, index) => {
       manifest.connectModules[`${type}:webhooks`][index].key = `webhook-${index + 1}`;
     });
+    console.log(` - Ensured all webhooks have automatically generated keys.`)
   }
 
   // Add scopes
-  connect.scopes.forEach(scope => {
-    console.log(`Scope: ${scope}`);
-    if (scope.toLocaleLowerCase() === 'act_as_user') {
-      warnings.push('ACT_AS_USER scope is not supported in Forge manifest.');
-    } else {
-      let forgeScope = scope.toLowerCase().replace('_', '-');
-      manifest.permissions.scopes.push(`${forgeScope}:connect-${type}`);
-    }
-  });
+  if(isPresent(connect.scopes) && connect.scopes.length > 0) {
+    connect.scopes.forEach(scope => {
+      if (scope.toLocaleLowerCase() === 'act_as_user') {
+        warnings.push('ACT_AS_USER scope is not supported in Forge manifest.');
+      } else {
+        let forgeScope = scope.toLowerCase().replace('_', '-');
+        manifest.permissions.scopes.push(`${forgeScope}:connect-${type}`);
+      }
+    });
+    console.log(` - Converted ${connect.scopes.length} connect scopes into correct format in manifest.`);
+  }
+
+  console.log('');
 
   return [manifest, warnings];
 }
